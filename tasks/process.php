494cd135e760c4844a3aca239917609c8e2f74a9
int13h <paul.halliday@gmail.com>
2012-07-11 07:13:30 -0300

    added license

--- tasks/process.php
+++ tasks/process.php
@@ -1,6 +1,28 @@
 #!/usr/local/bin/php
 <?php
 
+//
+//
+// This file is part of oculi
+//
+// Copyright (C) 2011-2012, Paul Halliday <paul.halliday@gmail.com>
+//                          Sacha Evans <sacha.evans@nscc.ca>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
 $debug = 0;
 $timestamp = date('Y-m-d H:i:s');
 $base = dirname(__FILE__);
